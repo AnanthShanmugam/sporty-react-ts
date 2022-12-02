2d47d6766ca7af71d4ae95c74b6f253c02349d07
Ananth Shanmugam <anshanmugam@informatica.com>
2022-12-02 18:59:26 -0500

    Added images and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "sportyreactts": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "NotesImages1": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
